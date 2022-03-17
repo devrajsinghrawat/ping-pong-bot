be6b3f23815690d770defd4e13723651c7433977
Devraj Singh <dsrawat@blockchains.com>
2022-03-17 23:35:45 +0100

    time syc fix

--- src/bot-pong.js
+++ src/bot-pong.js
@@ -32,7 +32,7 @@ async function startApp() {
     .on('data', async (event) => {
       try {
         console.log('Tx hash from event', event.blockNumber);
-        setTimeout(await postPongTx(event.transactionHash), 360000);   // 6 mins
+        setTimeout(await postPongTx(event.transactionHash), 520000);   // 6 mins
       } catch (error) {
         setTimeout(startApp, 520000);   // 9 mins 
       }
